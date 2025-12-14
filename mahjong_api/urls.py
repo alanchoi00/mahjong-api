74cc9e8046f6fbd26510e38fcbdce330d60defba
alanchoi00 <alanchoi.uni@gmail.com>
2025-12-14 23:52:25 +1100

    feat(api): update assets URL routing to remove 'api/' prefix

--- mahjong_api/urls.py
+++ mahjong_api/urls.py
@@ -35,5 +35,5 @@ urlpatterns = [
         'api-auth/',
         include('rest_framework.urls', namespace='rest_framework'),
     ),
-    path('api/assets/', include('assets.urls')),
+    path('assets/', include('assets.urls')),
 ]
